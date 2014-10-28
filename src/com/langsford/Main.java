159ddbaa7e5c3fe30c452047b17d352466d2a738
Trevyn Langsford <tlangsford@gmail.com>
2014-10-28 13:13:33 -0400

    Added resource files

--- src/com/langsford/Main.java
+++ src/com/langsford/Main.java
@@ -1,8 +1,18 @@
 package com.langsford;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
